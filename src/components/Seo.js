ead307884657143f6ccffc75151be6903f1e4f28
definite2 <edefne878@gmail.com>
2021-07-04 16:16:37 +0300

    add seo components

---
+++ src/components/Seo.js
@@ -0,0 +1,108 @@
+import { NextSeo, ArticleJsonLd } from "next-seo";
+import siteMetaData from "../configs/siteMetaData";
+export const SEO = {
+  title: siteMetaData.title,
+  description: siteMetaData.description,
+  openGraph: {
+    type: "website",
+    locale: siteMetaData.language,
+    url: siteMetaData.siteUrl,
+    title: siteMetaData.title,
+    description: siteMetaData.description,
+    images: [
+      {
+        url: `${siteMetaData.siteUrl}${siteMetaData.socialBanner}`,
+        alt: siteMetaData.title,
+        width: 1200,
+        height: 600,
+      },
+    ],
+  },
+  twitter: {
+    handle: siteMetaData.twitter,
+    site: siteMetaData.twitter,
+    cardType: "summary_large_image",
+  },
+  additionalMetaTags: [
+    {
+      name: "author",
+      content: siteMetaData.author,
+    },
+  ],
+};
+export const PageSeo = ({ title, description, url }) => {
+  return (
+    <NextSeo
+      title={`${title} – ${siteMetaData.title}`}
+      description={description}
+      canonical={url}
+      openGraph={{
+        url,
+        title,
+        description,
+      }}
+    />
+  );
+};
+export const BlogSeo = ({
+  title,
+  summary,
+  date,
+  lastmod,
+  url,
+  tags,
+  images = [],
+}) => {
+  const publishedAt = new Date(date).toISOString();
+  const modifiedAt = new Date(lastmod || date).toISOString();
+  let imagesArr =
+    images.length === 0
+      ? [siteMetaData.socialBanner]
+      : typeof images === "string"
+      ? [images]
+      : images;
+  const featuredImages = imagesArr.map((img) => {
+    return {
+      url: `${siteMetaData.siteUrl}${img}`,
+      alt: title,
+    };
+  });
+  return (
+    <>
+      <NextSeo
+        title={`${title} – ${siteMetaData.title}`}
+        description={summary}
+        canonical={url}
+        openGraph={{
+          type: "article",
+          article: {
+            publishedTime: publishedAt,
+            modifiedTime: modifiedAt,
+            authors: [`${siteMetaData.siteUrl}/about`],
+            tags,
+          },
+          url,
+          title,
+          description: summary,
+          images: featuredImages,
+        }}
+        additionalMetaTags={[
+          {
+            name: "twitter:image",
+            content: featuredImages[0].url,
+          },
+        ]}
+      />
+      <ArticleJsonLd
+        authorName={siteMetaData.author}
+        dateModified={modifiedAt}
+        datePublished={publishedAt}
+        description={summary}
+        images={featuredImages}
+        publisherName={siteMetaData.author}
+        title={title}
+        url={url}
+      />
+    </>
+  );
+};
