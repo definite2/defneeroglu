38699b3c5abdca1efbe697c2b3c39e55ea4e86e5
definite2 <edefne878@gmail.com>
2021-07-04 16:16:25 +0300

    add metadata for seo

---
+++ src/configs/siteMetadata.js
@@ -0,0 +1,16 @@
+const siteMetaData={
+  "title": "My Sweet Adventures",
+  "author": "Defne Eroğlu",
+  "headerTitle": "defneeroglu",
+  "description": "Personel website build with nextjs and tailwindcss",
+  "language": "en-us",
+  "siteUrl": "https://defneroglu.app",
+  "image": "/images/avatar.jpg",
+  "socialBanner": "/images/avatar.jpg",
+  "email": "edefne878@gmail.com",
+  "github": "https://github.com/definite2",
+  "twitter": "https://twitter.com/defne_erogluu",
+  "linkedin": "https://www.linkedin.com/in/defne-eroglu/",
+  "locale": "en-US"
+}
+export default siteMetaData;
