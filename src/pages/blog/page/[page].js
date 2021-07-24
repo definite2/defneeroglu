4b58a1423f9efc59c69f4f744a8c6b242ebb9d85
definite2 <edefne878@gmail.com>
2021-07-24 18:48:49 +0300

    add dynamic page for post

--- src/pages/blog/page/[page].js
+++ src/pages/blog/page/[page].js
@@ -4,8 +4,8 @@ import siteMetadata from "../../../constants/siteMetadata.json";
 import { getAllFilesFrontMatter } from "../../../utils/mdx";
 export const POSTS_PER_PAGE = 5;
 export const getStaticPaths = async () => {
-  const allPosts = await getAllFilesFrontMatter("blog");
-  const totalPages = Math.ceil(allPosts.length / POSTS_PER_PAGE);
+  const posts = await getAllFilesFrontMatter("blog");
+  const totalPages = Math.ceil(posts.length / POSTS_PER_PAGE);
   const paths = Array.from({ length: totalPages }, (_, i) => ({
     params: { page: (i + 1).toString() },
   }));
@@ -16,16 +16,16 @@ export const getStaticPaths = async () => {
 };
 export const getStaticProps = async (context) => {
   const{params:{page}}=context;
-  const allPosts=await getAllFilesFrontMatter('blog');
+  const posts=await getAllFilesFrontMatter('blog');
   const pageNumber=parseInt(page);
-  const initialDisplayPosts=allPosts.slice(POSTS_PER_PAGE*(pageNumber-1),POSTS_PER_PAGE*pageNumber);
+  const initialDisplayPosts=posts.slice(POSTS_PER_PAGE*(pageNumber-1),POSTS_PER_PAGE*pageNumber);
   const pagination={
     currentPage=pageNumber,
-    totalPages:Math.ceil(allPosts.length/POSTS_PER_PAGE)
+    totalPages:Math.ceil(posts.length/POSTS_PER_PAGE)
   };
   return{
     props:{
-      posts:allPosts,
+      posts,
       initialDisplayPosts,
       pagination
     }
