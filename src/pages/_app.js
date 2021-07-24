f56d92a49c38f38e7f02036df7ab2876acbea938
definite2 <edefne878@gmail.com>
2021-07-24 14:23:49 +0300

    use sass instead of css

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,8 +2,8 @@ import { ThemeProvider } from "next-themes";
 import { DefaultSeo } from "next-seo";
 import Head from "next/head";
 import { SEO } from "../components/Seo";
-import "../styles/global.css";
-import Layout from "../components/Layout";
+import "../styles/global.scss";
+import Wrapper from "../components/Layout/Wrapper";
 const App =  ({ Component, pageProps })=> {
   return (
     <ThemeProvider attribute="class">
@@ -11,9 +11,9 @@ const App =  ({ Component, pageProps })=> {
         <meta content="width=device-width, initial-scale=1" name="viewport" />
       </Head>
       <DefaultSeo {...SEO} />
-      <Layout>
+      <Wrapper>
         <Component {...pageProps} />
-      </Layout>
+      </Wrapper>
     </ThemeProvider>
   );
 }
