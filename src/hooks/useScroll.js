d6a63e7c8986fc9c20d8e6080e515ae93003af3d
definite2 <edefne878@gmail.com>
2021-08-12 11:18:37 +0300

    add scroll hook

---
+++ src/hooks/useScroll.js
@@ -0,0 +1,19 @@
+import { useEffect, useState } from "react";
+import { off, on } from "@/utils/ui";
+
+const useScrolling = (ref) => {
+  const [scrolling, setScrolling] = useState(false);
+  const handleScroll = () => {
+    const isScrolled = window.pageYOffset > 0;
+    setScrolling(isScrolled);
+  };
+  useEffect(() => {
+    on(window, "scroll", handleScroll, { passive: true });
+    return () => {
+      off(window, "scroll", handleScroll, { passive: true });
+    };
+  }, [ref]);
+  return scrolling;
+};
+
+export default useScrolling;
