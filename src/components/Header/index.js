b8f37efc00be1f93952c5216daf07ac953a56f9f
definite2 <edefne878@gmail.com>
2021-08-12 20:14:57 +0300

    upate header nav component

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -6,44 +6,45 @@ import navLinks from "@/constants/navLinks";
 import React, { useRef } from "react";
 import Logo from "./Avatar";
 import useScrolling from "@/hooks/useScroll";
-const Wrapper = styled.header`
-  display: flex;
-  justify-content: space-between;
+const Wrapper = styled.nav`
   position: fixed;
-  align-items: center;
   top: 0;
   left: 0;
   width: 100%;
   border: none;
   box-shadow: none;
-  padding: 2rem 0;
-  background-color: white;
-  transition: box-shadow 250ms ease-in-out, background 250ms ease-in-out;
+  background-color: ${(props) => props.theme.background};
+  transition: box-shadow 250ms ease-in-out;
   ${(props) =>
     props.isScrolled &&
     css`
-    background-color: #fff;
       box-shadow: rgba(0, 0, 0, 0.15) 0px 1px 4px 0px;
       z-index: 9;
     `};
 `;
+
 const Header = () => {
   const reff = useRef(null);
   const scrolled = useScrolling(reff);
   return (
-    <Wrapper isScrolled={scrolled}>
-      <Logo />
-      <div className="flex items-center text-base leading-5">
-        <div className="hidden sm:block">
-          {navLinks.map((route, i) => (
-            <NavLink key={i} href={route.href}>
-              {route.title}
-            </NavLink>
-          ))}
+    <Wrapper
+      isScrolled={scrolled}
+      className="flex items-center justify-between  bg-white dark:bg-gray-900 py-5"
+    >
+      <div className="w-full justify-between flex xl:max-w-5xl my-0 mx-auto py-1 lg:m ">
+        <Logo />
+        <div className="flex justify-between items-center text-base leading-5">
+          <div className="hidden sm:block">
+            {navLinks.map((route, i) => (
+              <NavLink key={i} href={route.href}>
+                {route.title}
+              </NavLink>
+            ))}
+          </div>
+          <ThemeToggle />
         </div>
-        <ThemeToggle />
-        <MobileNavbar />
       </div>
+      <MobileNavbar />
     </Wrapper>
   );
 };
