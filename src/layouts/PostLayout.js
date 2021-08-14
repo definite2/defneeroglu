0c47f8eb2839de993b6388876e569110dfc5033b
definite2 <edefne878@gmail.com>
2021-08-14 11:01:35 +0300

    add reasing time from frontmatter

--- src/layouts/PostLayout.js
+++ src/layouts/PostLayout.js
@@ -3,37 +3,40 @@ import PageTitle from "@/components/PageTitle";
 import SectionContainer from "@/components/SectionContainer";
 import { BlogSeo } from "@/components/Seo";
 import siteMetadata from "@/constants/siteMetadata.json";
+const editUrl = (fileName) =>
+  `${siteMetadata.siteRepo}/blob/master/data/blog/${fileName}`;
+const discussUrl = (slug) =>
+  `https://mobile.twitter.com/search?q=${encodeURIComponent(
+    `${siteMetadata.siteUrl}/blog/${slug}`
+  )}`;
 
-export default function PostLayout({
-  frontMatter,
-  authorDetails,
-  next,
-  prev,
-  children,
-}) {
-  const { date, title } = frontMatter;
+const postDateTemplate = { year: "numeric", month: "long", day: "numeric" };
+export default function PostLayout({ frontMatter, next, prev, children }) {
+  const { slug, fileName, date, title, tags } = frontMatter;
 
   return (
     <SectionContainer>
-      <BlogSeo
-        url={`${siteMetadata.siteUrl}/blog/${frontMatter.slug}`}
-        {...frontMatter}
-      />
+      <BlogSeo url={`${siteMetadata.siteUrl}/blog/${slug}`} {...frontMatter} />
       <article>
-        <div>
-          <header>
+        <div className="xl:divide-y xl:divide-gray-200 xl:dark:divide-gray-700">
+          <header className="pt-6 xl:pb-6">
             <div className="pb-10 space-y-1 text-center border-b border-gray-200 dark:border-gray-700">
-              <dl>
-                <div>
-                  <dt className="sr-only">Published on</dt>
-                  <dd className="text-base font-medium leading-6 text-gray-500 dark:text-gray-400">
-                    <time dateTime={date}>{date}</time>
-                  </dd>
-                </div>
-              </dl>
-              <div>
+              <div className="mb-6">
                 <PageTitle>{title}</PageTitle>
               </div>
+              <dl>
+                <div className="flex text-base font-medium leading-6 text-gray-500 dark:text-gray-400">
+                  <dt className="sr-only">Published on</dt>
+                  <dd>
+                    {new Date(date).toLocaleDateString(
+                      siteMetadata.locale,
+                      postDateTemplate
+                    )}
+                  </dd>
+                  <span class="dot"></span>
+                  <span>{frontMatter.readingTime.text}</span>
+                </div>
+              </dl>
             </div>
           </header>
           <div
@@ -41,7 +44,7 @@ export default function PostLayout({
             style={{ gridTemplateRows: "auto 1fr" }}
           >
             <div className="divide-y divide-gray-200 dark:divide-gray-700 xl:pb-0 xl:col-span-3 xl:row-span-2">
-              <div className="pt-10 pb-8 prose dark:prose-dark max-w-none">
+              <div className="pt-10 pb-8 prose dark:prose-dark max-w-none font-sans  font-medium">
                 {children}
               </div>
             </div>
