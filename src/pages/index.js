e6b98fc25478f0f7012c9d5912562fcf97916d8c
definite2 <edefne878@gmail.com>
2021-08-21 15:20:17 +0300

    make all card as link elemnt

--- src/pages/index.js
+++ src/pages/index.js
@@ -34,48 +34,45 @@ export default function Home({ posts }) {
             return (
               <li key={slug} className="pt-6">
                 <article>
-                  <div className="flex flex-col items-center justify-between h-full overflow-hidden cursor-pointer transition-all duration-200 ease-in-out transform border-2 border-gray-100 rounded-lg hover:scale-105 hover:shadow-xl">
-                    <div className="relative w-full">
-                      <header className="relative pb-1/2">
-                        <Image
-                          layout="fill"
-                          className="absolute top-0 left-0 object-cover w-full h-full"
-                          src={images[0]}
-                          alt={title}
-                        />
-                      </header>
-                    </div>
-                    <dl>
-                      <dt className="sr-only"> Published on</dt>
-                      <dd className="text-base  leading-6 text-gray-400 dark:text-gray-400">
-                        <time dateTime={date}>
-                          {' '}
-                          {new Date(date).toLocaleDateString(
-                            siteMetadata.locale,
-                            postDateTemplate
-                          )}{' '}
-                        </time>
-                      </dd>
-                    </dl>
-                    <section className="flex flex-col h-full px-5 py-5 sm:px-8 sm:py-10">
-                      <h2 className="pb-5 text-xl font-semibold leading-tight sm:text-2xl">
-                        <CustomLink
-                          href={`/blog/${slug}`}
-                          className="text-gray-800 dark:text-gray-100"
-                        >
+                  <CustomLink href={`/blog/${slug}`} className="text-gray-800 dark:text-gray-100">
+                    <div className="flex flex-col items-center justify-between h-full overflow-hidden cursor-pointer transition-all duration-200 ease-in-out transform border-2 border-gray-100 rounded-lg hover:scale-105 hover:shadow-xl">
+                      <div className="relative w-full">
+                        <header className="relative pb-1/2">
+                          <Image
+                            layout="fill"
+                            className="absolute top-0 left-0 object-cover w-full h-full"
+                            src={images[0]}
+                            alt={title}
+                          />
+                        </header>
+                      </div>
+                      <dl>
+                        <dt className="sr-only"> Published on</dt>
+                        <dd className="text-base  leading-6 text-gray-400 dark:text-gray-400">
+                          <time dateTime={date}>
+                            {' '}
+                            {new Date(date).toLocaleDateString(
+                              siteMetadata.locale,
+                              postDateTemplate
+                            )}{' '}
+                          </time>
+                        </dd>
+                      </dl>
+                      <section className="flex flex-col h-full px-5 py-5 sm:px-8 sm:py-10">
+                        <h2 className="pb-5 text-xl font-semibold leading-tight sm:text-2xl">
                           {title}
-                        </CustomLink>
-                      </h2>
-                      <div className="flex flex-wrap">
-                        {tags.map((tag) => (
-                          <Tag key={tag} text={tag} />
-                        ))}
-                      </div>
-                      <div className="prose transition-opacity duration-200 ease-in-out opacity-75 hover:opacity-100 text-gray-500 max-w-none dark:text-gray-400">
-                        <p>{summary}</p>
-                      </div>
-                    </section>
-                  </div>
+                        </h2>
+                        <div className="flex flex-wrap">
+                          {tags.map((tag) => (
+                            <Tag key={tag} text={tag} />
+                          ))}
+                        </div>
+                        <div className="prose transition-opacity duration-200 ease-in-out opacity-75 hover:opacity-100 text-gray-500 max-w-none dark:text-gray-400">
+                          <p>{summary}</p>
+                        </div>
+                      </section>
+                    </div>
+                  </CustomLink>
                 </article>
               </li>
             )
