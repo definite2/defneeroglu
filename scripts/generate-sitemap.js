fbdc88a370e661647e42a5d143248598cc77d0ba
definite2 <edefne878@gmail.com>
2021-08-25 19:43:33 +0300

    folder rename

--- scripts/generate-sitemap.js
+++ scripts/generate-sitemap.js
@@ -1,7 +1,7 @@
 const fs = require('fs')
 const globby = require('globby')
 const prettier = require('prettier')
-const siteMetadata = require('../constants/siteMetadata')
+const siteMetadata = require('../src/constants/siteMetadata')
 
 ;(async () => {
   const prettierConfig = await prettier.resolveConfig('./.prettierrc.js')
