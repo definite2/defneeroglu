b290f81e8ec06491f5408363875be020461bb0e7
definite2 <edefne878@gmail.com>
2021-08-27 21:07:46 +0300

    add scroll to top

---
+++ src/components/ScrollTop/index.js
@@ -0,0 +1,49 @@
+import Image from "next/image";
+import scroll from "./scrolltotop.svg";
+import useScrolling from "@/hooks/useScroll";
+import styled, { css } from "styled-components";
+export default function ScrollToTop() {
+  const shouldShow = (props) => {
+    if (props.scrolled) {
+      if (props.showOnScroll) return true;
+      if (props.hideOnScroll) return false;
+    }
+    if (props.showOnScroll) return false;
+    return true;
+  };
+
+  const Scroll = styled.div`
+    transition: opacity 500ms ease-in-out,transform 1000ms ease-in-out;
+
+    ${(props) => {
+      if (shouldShow(props)) {
+        return css`
+          opacity: 1;
+          transform: translateY(0);
+        `;
+      }
+      return css`
+        opacity: 0;
+        transform: translateY(-16px);
+      `;
+    }};
+  `;
+  const isscrolled = useScrolling(800);
+
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  };
+  return (
+    <Scroll
+      scrolled={isscrolled}
+      className=" overflow-hidden fixed bottom-4 cursor-pointer right-8 transform-transition hover:-translate-y-2 "
+      onClick={scrollToTop}
+      showOnScroll
+    >
+      <Image src={scroll} width="48" height="48" color="pink" />
+    </Scroll>
+  );
+}
