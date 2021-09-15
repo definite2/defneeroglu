68b67dd1a140d1a1aca78e63cf96139d6d7a95be
definite2 <edefne878@gmail.com>
2021-09-15 09:11:46 +0300

    add tags page

--- src/components/Tag.js
+++ src/components/Tag.js
@@ -4,7 +4,7 @@ import kebabCase from "lib/kebapCase";
 const Tag = ({ text }) => {
   return (
     <Link href={`/tags/${kebabCase(text)}`}>
-      <a className="flex items-center mr-3 text-sm font-medium uppercase  text-indigo-600 hover:text-indigo-500 dark:hover:text-indigo-500">
+      <a className="flex items-center mr-3 text-sm font-medium uppercase  text-primary-600 hover:text-primary-500 dark:hover:text-primary-500">
         {text.split(" ").join("-")}
       </a>
     </Link>
