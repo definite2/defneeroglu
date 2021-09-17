18c76f82ccae0ac74366a635e63601675c7c0eee
definite2 <edefne878@gmail.com>
2021-09-17 19:52:10 +0300

    less margin bottom

--- src/components/SectionContainer.js
+++ src/components/SectionContainer.js
@@ -1,3 +1,3 @@
 export default function SectionContainer({ children }) {
-  return <div className="grid grid-cols-10 gap-1 mx-auto mb-36">{children}</div>
+  return <div className="grid grid-cols-10 gap-1 mx-auto mb-12">{children}</div>
 }
