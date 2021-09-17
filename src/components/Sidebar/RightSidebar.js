719110c942fb16791736ae8cb182609c3662e7c4
definite2 <edefne878@gmail.com>
2021-09-17 19:55:33 +0300

    update right sidebar fixed to sticky

--- src/components/Sidebar/RightSidebar.js
+++ src/components/Sidebar/RightSidebar.js
@@ -9,12 +9,7 @@ const SS = styled.span`
   height: 25px;
   background-color: #e2e2e1;
 `;
-const RightSidebar = () => {
-  const items = [
-    { href: "/deneme", name: "Deneme" },
-    { href: "/deneme2", name: "Deneme2" },
-    { href: "/deneme3", name: "Deneme3" },
-  ];
+const RightSidebar = ({ items }) => {
   const items2 = [
     { href: "/denemee", name: "Deneme" },
     { href: "/denemee2", name: "Deneme2" },
@@ -22,29 +17,30 @@ const RightSidebar = () => {
   ];
   return (
     <aside className="md:col-span-2 md:col-start-9 col-span-10 px-16 md:mt-80 max-h-screen border-0">
-      <div className="fixed top-40 rounded-xl w-full h-full shadow-md p-4">
+      <div className="sticky top-40 rounded-xl w-full h-full shadow-md p-4">
         <div className="divide-y divide-gray-200 dark:divide-gray-700">
           <h6 className="pb-2 relative text-2xl tracking-tighter">
             Categories
-            <SS/>
+            <SS />
           </h6>
-        
+
           <ul className="flex flex-col overflow-hidden divide-y divide-gray-200 dark:divide-gray-700">
-            {items.map((item, idx) => (
+            {items.slice(0, 5).map((frontmatter) => {
+              const { slug, date, title, summary, tags, images } = frontmatter;
               <SidebarItem
-                key={`sidebar-item-${idx}`}
-                href={item.href}
-                name={item.name}
-              />
-            ))}
-          </ul>     
+                key={`sidebar-item-${title}`}
+                href={`/blog/${slug}`}
+                name={title}
+              />;
+            })}
+          </ul>
         </div>
         <div className="divide-y divide-gray-200 dark:divide-gray-700 mt-12">
           <h6 className="pb-2 relative text-2xl tracking-tighter">
             Related Posts
-            <SS/>
+            <SS />
           </h6>
-          
+
           <ul className="flex flex-col overflow-hidden divide-y divide-gray-200 dark:divide-gray-700">
             {items2.map((item, idx) => (
               <SidebarItem
