6d6d6e98b358a2d517a90ff4296728365bf76405
definite2 <edefne878@gmail.com>
2021-09-17 19:51:50 +0300

    add common input

--- src/layouts/ListLayout.js
+++ src/layouts/ListLayout.js
@@ -3,6 +3,7 @@ import CustomLink from "@/components/CustomLink";
 import Pagination from "@/components/Pagination";
 import { formatDate } from "lib/date";
 import Tag from "@/components/Tag";
+import Input from "@/components/Forms/Input";
 //TODO paginationa later
 const ListLayout = ({ posts, title, initialDisplayPosts = [], pagination }) => {
   const [searchValue, setSearchValue] = useState("");
@@ -19,25 +20,25 @@ const ListLayout = ({ posts, title, initialDisplayPosts = [], pagination }) => {
           <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
             {title}
           </h1>
-          <div className="relative max-w-lg">
-            <input
+          <div className="flex items-center max-w-lg">
+            <Input
               aria-label="Search articles"
               type="text"
               onChange={(e) => setSearchValue(e.target.value)}
               placeholder="Search articles"
-              className="block w-full lg:w-1/2 px-4 py-2 text-gray-900 bg-white border-gray-300 focus:outline-none  rounded-lg dark:border-gray-900 focus:ring-indigo-400 focus:border-indigo-400 dark:bg-gray-800 dark:text-gray-100"
+             
             />
             <svg
-              className="absolute w-5 h-5 text-gray-400 right-2 top-2 dark:text-gray-300"
               xmlns="http://www.w3.org/2000/svg"
+              className="w-6 h-6 -ml-8 text-gray-400"
               fill="none"
               viewBox="0 0 24 24"
               stroke="currentColor"
             >
               <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth={2}
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
                 d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
               />
             </svg>
@@ -58,9 +59,12 @@ const ListLayout = ({ posts, title, initialDisplayPosts = [], pagination }) => {
                     </dd>
                   </dl>
                   <div className="space-y-3 xl:col-span-3">
-                  <div>
+                    <div>
                       <h3 className="text-2xl font-bold leading-8 tracking-tight">
-                        <CustomLink href={`/blog/${slug}`} className="text-gray-900 dark:text-gray-100">
+                        <CustomLink
+                          href={`/blog/${slug}`}
+                          className="text-gray-900 dark:text-gray-100"
+                        >
                           {title}
                         </CustomLink>
                       </h3>
@@ -81,7 +85,10 @@ const ListLayout = ({ posts, title, initialDisplayPosts = [], pagination }) => {
         </ul>
       </div>
       {pagination && pagination.totalPages > 1 && !searchValue && (
-        <Pagination currentPage={pagination.currentPage} totalPages={pagination.totalPages} />
+        <Pagination
+          currentPage={pagination.currentPage}
+          totalPages={pagination.totalPages}
+        />
       )}
     </>
   );
