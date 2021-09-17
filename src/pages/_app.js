ee4a9069f5f46fdd7ebe065d03e38cc2fb4db2d5
definite2 <edefne878@gmail.com>
2021-09-17 19:52:24 +0300

    rename path

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -3,7 +3,7 @@ import { DefaultSeo } from "next-seo";
 import Head from "next/head";
 import { SEO } from "@/components/Seo";
 import "../styles/global.scss";
-import Wrapper from "@/components/Wrapper";
+import Wrapper from "@/components/Layout";
 import { GlobalStyle } from "styles/GlobalStyle";
 import Analytics from "@/components/Analytics";
 
