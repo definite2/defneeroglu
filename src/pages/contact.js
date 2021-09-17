c822eda7bd372626f0f786dbfc5d887603d75ff2
definite2 <edefne878@gmail.com>
2021-09-17 19:53:52 +0300

    no need avat in contact form

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,8 +1,6 @@
 import { PageSeo } from "@/components/Seo";
 import { siteMetadata } from "@/constants/siteMetadata";
-import Image from "next/image";
-import SocialIcon from "@/components/Social";
-import avatar from "../../public/media/avatar1.jpg";
+import Avatar from "@/components/Avatar";
 import ContactForm from "@/components/Forms/ContactForm";
 const contact = () => {
   const { author, occupation, email, twitter, linkedin, github } = siteMetadata;
@@ -14,13 +12,14 @@ const contact = () => {
       />
       <div className="divide-y">
         <div className="pt-6 pb-8 space-y-2 md:space-y-5">
-          <h1 className="text-2xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-3xl sm:leading-10 md:text-4xl md:leading-14">
-            GET IN TOUCH
+          <h1 className="text-2xl font-bold leading-9 tracking-tight text-gray-800 dark:text-gray-100 sm:text-3xl sm:leading-10 md:text-4xl md:leading-14">
+            Get In Touch
           </h1>
         </div>
         <div className="items-start space-y-2 xl:grid xl:grid-cols-3 xl:gap-x-8 xl:space-y-0 lg:pt-8 ">
-        <div className="flex flex-col items-center pt-8 space-x-2 ">
-          Plaese send me message bitches
+          <div className="flex flex-col items-center pt-8 space-x-2 ">
+       
+            <p className="mt-12">I would love to hear from you 🙂</p>
           </div>
           <div className="pt-8 pb-8 prose lg:prose-lg dark:prose-dark max-w-none xl:col-span-2">
             <ContactForm />
