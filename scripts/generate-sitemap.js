2aa6970f073c8464f29b2a1dd4916bd07a75f806
definite2 <edefne878@gmail.com>
2021-09-18 22:31:17 +0300

    update generate stiemap

--- scripts/generate-sitemap.js
+++ scripts/generate-sitemap.js
@@ -1,7 +1,7 @@
-import fs from "fs"
-import { globby } from "globby"
-import { siteMetadata } from "@/constants/siteMetadata"
-;(async () => {
+import { writeFileSync } from "fs";
+import { globby } from "globby";
+import { siteMetadata } from "@/constants/siteMetadata";
+(async () => {
   const pages = await globby([
     "pages/*.js",
     "_content/blog/**/*.mdx",
@@ -9,7 +9,7 @@ import { siteMetadata } from "@/constants/siteMetadata"
     "public/tags/**/*.xml",
     "!pages/_*.js",
     "!pages/api",
-  ])
+  ]);
 
   const sitemap = `
         <?xml version="1.0" encoding="UTF-8"?>
@@ -23,21 +23,24 @@ import { siteMetadata } from "@/constants/siteMetadata"
                   .replace(".js", "")
                   .replace(".mdx", "")
                   .replace(".md", "")
-                  .replace("/feed.xml", "")
-                const route = path === "/index" ? "" : path
-                if (page === `pages/404.js` || page === `pages/blog/[...slug].js`) {
-                  return
+                  .replace("/feed.xml", "");
+                const route = path === "/index" ? "" : path;
+                if (
+                  page === `pages/404.js` ||
+                  page === `pages/blog/[...slug].js`
+                ) {
+                  return;
                 }
                 return `
                         <url>
                             <loc>${siteMetadata.siteUrl}${route}</loc>
                         </url>
-                    `
+                    `;
               })
               .join("")}
         </urlset>
-    `
+    `;
 
   // eslint-disable-next-line no-sync
-  fs.writeFileSync("public/sitemap.xml", sitemap)
-})()
+  writeFileSync("public/sitemap.xml", sitemap);
+})();
