874cf3ba56de680d84454379d9aa6aa48089984d
definite2 <edefne878@gmail.com>
2021-09-20 20:41:48 +0300

    increase main col span

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -3,13 +3,14 @@ import Header from "@/components/Header";
 import ScrollToTop from "@/components/ScrollTop";
 import Footer from "@/components/Footer";
 const Wrapper = ({ children }) => {
+  console.log(children.layout)
   return (
     <>
       <SectionContainer>
         <Header />
         <main
           role="main"
-          className="md:col-span-6 md:col-start-4 col-span-12 px-5 my-40 overflow-auto"
+          className="md:col-span-7 md:col-start-4 col-span-12 px-5 my-40"
         >
           {children}
         </main>
