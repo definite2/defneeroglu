6527395d39669483ab1ded5724240e52e02dec7b
definite2 <edefne878@gmail.com>
2021-09-24 19:19:02 +0300

    update alert dialog

--- src/components/AlertDialog/index.js
+++ src/components/AlertDialog/index.js
@@ -24,56 +24,47 @@ const alertVariants = {
   },
 }
 
-const AlertDialog = ({ handleClose, title, message, success }) => {
+const AlertDialog = ({ handleClose, message, title, success }) => {
   return (
     <Backdrop onClick={handleClose}>
-      <div className="items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center block sm:p-0">
-        <div
-          className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity"
-          aria-hidden="true"
-        ></div>
-
-        <span className="hidden sm:inline-block sm:align-middle sm:h-screen" aria-hidden="true">
-          &#8203;
-        </span>
-        <motion.div
-          onClick={(e) => e.stopPropagation()}
-          className={`inline-block align-bottom bg-primary-light dark:bg-gray-800 rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full`}
-          variants={alertVariants}
-          initial="hidden"
-          animate="visible"
-          exit="exit"
-        >
-          <div className="bg-primary-light dark:bg-gray-800 px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
-            <div className="sm:flex sm:items-start">
-              <div
-                className={`m-auto flex-shrink-0 flex items-center justify-center h-12 w-12 rounded-full ${
-                  success ? 'bg-primary-100' : 'bg-red-100'
-                } sm:mx-0 sm:h-10 sm:w-10`}
-              >
-                {success ? (
-                  <Check className="text-primary-500" />
-                ) : (
-                  <AlertTriangle className="text-red-500" />
-                )}
-              </div>
-              <div className="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
-                <h3
-                  className="text-lg leading-6 font-medium text-gray-900 dark:text-gray-50"
-                  id="alert-dialog-title"
-                >
-                  {title}
-                </h3>
-                <div className="mt-2">
-                  <p id="alert-description" className="text-sm text-gray-00 dark:text-gray-100">
-                    {message}
-                  </p>
-                </div>
-              </div>
+      <motion.div
+        onClick={(e) => e.stopPropagation()}
+        className="inline-block align-bottom bg-primary-grayish dark:bg-gray-800 rounded-lg text-left overflow-hidden shadow-xl sm:my-8 sm:align-middle sm:max-w-lg sm:w-full"
+        variants={alertVariants}
+        initial="hidden"
+        animate="visible"
+        exit="exit"
+      >
+          
+          <div className="bg-primary-grayish dark:bg-gray-800 px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
+        <div className="sm:flex sm:items-start">
+          <div
+            className={`m-auto flex-shrink-0 flex items-center justify-center h-12 w-12 rounded-full ${
+              success ? 'bg-primary-100' : 'bg-red-100'
+            } sm:mx-0 sm:h-10 sm:w-10`}
+          >
+            {success ? (
+              <Check className="text-primary-500" />
+            ) : (
+              <AlertTriangle className="text-red-500" />
+            )}
+          </div>
+          <div className="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
+            <h3
+              className="text-lg leading-6 font-medium text-gray-900 dark:text-gray-50"
+              id="alert-dialog-title"
+            >
+              {title}
+            </h3>
+            <div className="mt-2">
+              <p id="alert-description" className="text-sm text-gray-00 dark:text-gray-100">
+                {message}
+              </p>
             </div>
           </div>
-        </motion.div>
-      </div>
+        </div>
+        </div>
+      </motion.div>
     </Backdrop>
   )
 }
