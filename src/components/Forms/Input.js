154c91ca5337c854ffc6956fd7fba069333b1698
definite2 <edefne878@gmail.com>
2021-09-24 19:22:46 +0300

    update stylings

--- src/components/Forms/Input.js
+++ src/components/Forms/Input.js
@@ -14,9 +14,9 @@ const Input = (props) => {
         type={type}
         onChange={onChange}
         value={value}
-        className="appearance-none w-full border-primary-100  text-gray-700  py-2 px-2 rounded-md leading-tight focus:outline-none focus:ring-primary-200 focus:border-primary-200 shadow-sm dark:bg-gray-800 dark:text-gray-100"
+        className="appearance-none w-full border-primary-300  text-gray-700  py-4 px-2 rounded-md leading-tight focus:outline-none focus:ring-primary-500 focus:border-primary-500 shadow-sm dark:bg-gray-800 dark:text-gray-100"
       />
-      {error && <span className="text-xs text-red-700">{error}</span>}
+      {error && <span className="text-xs text-red-600">{error}</span>}
     </>
   );
 };
