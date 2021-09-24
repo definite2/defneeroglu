91d695aac91959b3632252bf6419df4d813f75f3
definite2 <edefne878@gmail.com>
2021-09-24 19:17:50 +0300

    update contact form colors, show new spring alert

--- src/components/Forms/ContactForm.js
+++ src/components/Forms/ContactForm.js
@@ -1,80 +1,92 @@
-import { useState } from "react";
+import { useState } from 'react'
 //TODO add email validation
-import Input from "./Input";
-import Alert from "../Alert";
-import { motion } from "framer-motion";
-import { useForm } from "@/hooks/useForm";
-const ContactForm = () => {
-  const initialFValues = {
-    name: "",
-    email: "",
-    message: "",
-  };
-  const validate = (fieldValues = values) => {
-    let temp = { ...errors };
-    if ("email" in fieldValues)
-      temp.email = /$^|.+@.+..+/.test(fieldValues.email)
-        ? ""
-        : "Email is not valid.";
-    if ("message" in fieldValues) {
-      temp.message = fieldValues.message
-        ? ""
-        : "Your message should not be empty";
-      temp.message =
-        fieldValues.message.length > 1001 ? "Too long message!" : "";
-    }
+import Input from './Input'
+import AlertDialog from '../AlertDialog'
+import { AlertContainer } from '../AlertDialog/AlertContainer'
+import { motion } from 'framer-motion'
+import { useForm } from '@/hooks/useForm'
+import useAlert from '@/hooks/useAlert'
 
+const initialFValues = {
+  name: '',
+  email: '',
+  message: '',
+}
+const ContactForm = () => {
+  const [alertMessage, setAlertMessage] = useState('')
+  const [success, setSuccess] = useState(null)
+  const { open, close, alertDialogOpen } = useAlert()
+
+  const validate = (fieldValues = values) => {
+    let temp = { ...errors }
+    if ('name' in fieldValues) temp.name = fieldValues.name ? '' : 'Your name should not be empty'
+    if ('email' in fieldValues) {
+      if (!fieldValues.email) temp.email = 'Your email should not be empty'
+      else if (!/$^|.+@.+..+/.test(fieldValues.email)) {
+        temp.email = 'Please provide a valid email address'
+      } else {
+        temp.email = ''
+      }
+    }
+    if ('message' in fieldValues) {
+      if (!fieldValues.message) {
+        temp.message = 'Your message should not be empty'
+      } else if (fieldValues.message.length > 1001) {
+        temp.message = 'Too long message!'
+      } else {
+        temp.message = ''
+      }
+    }
     setErrors({
       ...temp,
-    });
+    })
 
-    if (fieldValues == values) return Object.values(temp).every((x) => x == "");
-  };
+    if (fieldValues == values) return Object.values(temp).every((x) => x == '')
+  }
   const { values, errors, setErrors, handleInputChange, resetForm } = useForm(
     initialFValues,
     true,
     validate
-  );
-  const [alertMessage, setAlertMessage] = useState("");
-  const [showAlert, setShowAlert] = useState(false);
-  const [success, setSuccess] = useState(null);
+  )
 
   const handleSubmit = async (e) => {
-    e.preventDefault();
+    e.preventDefault()
     if (validate()) {
-      fetch("/api/contact", {
-        method: "POST",
+      fetch('/api/contact', {
+        method: 'POST',
         headers: {
-          Accept: "application/json, text/plain, */*",
-          "Content-Type": "application/json",
+          Accept: 'application/json, text/plain, */*',
+          'Content-Type': 'application/json',
         },
         body: JSON.stringify(values),
       })
         .then((res) => {
-          setShowAlert(true);
+          open()
           if (res.status === 200) {
-            setAlertMessage("Thank you for reaching me out");
-            setSuccess(true);
+            setAlertMessage('Thank you for reaching me out')
+            setSuccess(true)
           }
         })
         .catch((err) => {
-          setAlertMessage(err.message);
-          setSuccess(false);
-        });
-      resetForm();
+          setAlertMessage(err.message)
+          setSuccess(false)
+        })
+      resetForm()
     }
-  };
+  }
   return (
     <>
-      {showAlert && (
-        <Alert
-          show={showAlert}
-          setShow={setShowAlert}
-          message={alertMessage}
-          success={success}
-          title={success ? "Thank you!" : "Something went wrong"}
-        />
-      )}
+      <AlertContainer>
+        {alertDialogOpen && (
+          <AlertDialog
+            alertDialogOpen={alertDialogOpen}
+            message={alertMessage}
+            success={success}
+            title={success ? 'Thank you!' : 'Something went wrong'}
+            handleClose={close}
+          />
+        )}
+      </AlertContainer>
       <form className="w-full">
         <div className="flex flex-wrap mb-6">
           <div className="w-full md:w-2/3 ">
@@ -105,31 +117,27 @@ const ContactForm = () => {
             </label>
             <textarea
               id="message"
-              className="no-resize block w-full appearance-none rounded-md border-primary-100  text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none focus:ring-primary-200 focus:border-primary-200 h-36 resize-none shadow-sm dark:bg-gray-800 dark:text-gray-100"
+              className="no-resize block w-full appearance-none rounded-md border-primary-300  text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none focus:ring-primary-400 focus:border-primary-400 h-36 resize-none shadow-sm dark:bg-gray-800 dark:text-gray-100"
               onChange={handleInputChange}
               value={values.message}
               error={errors.message}
             />
-            {errors.message && (
-              <span class="text-xs text-red-700">{errors.message}</span>
-            )}
+            {errors.message && <span class="text-xs text-red-600">{errors.message}</span>}
           </div>
         </div>
         <div className="flex flex-wrap mb-6">
           <motion.button
             whileHover={{ scale: 1.1 }}
             whileTap={{ scale: 0.9 }}
-            className="  bg-primary-100 flex items-center justify-center font-medium  border-0 p-2 rounded-md shadow-sm"
+            className="  bg-gradient-to-r from-orange-400 to-primary-400 flex items-center justify-center font-medium  border-0 p-2 rounded-md shadow-sm"
             onClick={handleSubmit}
           >
-            <span className="px-1 text-transparent bg-clip-text bg-gradient-to-br from-orange-400  to-primary-400">
-              Submit
-            </span>
+            <span className="px-3  text-primary-light dark:text-gray-900">Send</span>
           </motion.button>
         </div>
       </form>
     </>
-  );
-};
+  )
+}
 
-export default ContactForm;
+export default ContactForm
