5711aecd225868b0692d4bce77fba2e625a20bdc
definite2 <edefne878@gmail.com>
2021-09-24 20:21:29 +0300

    update TOC

--- src/components/mdx/TOC.js
+++ src/components/mdx/TOC.js
@@ -1,29 +1,27 @@
-import { useState, useEffect } from "react";
-import useIntersectNode from "@/hooks/useIntersection";
-import clsx from "clsx";
-import CustomLink from "../CustomLink";
+import { useState, useEffect } from 'react'
+import useIntersectNode from '@/hooks/useIntersection'
+import clsx from 'clsx'
+import CustomLink from '../CustomLink'
 
 export function TOC() {
-  const [headings, setHeadings] = useState([]);
+  const [headings, setHeadings] = useState([])
 
   useEffect(() => {
-    const afterElm = document.getElementById("post-body");
-    const headingElements = Array.from(
-      afterElm.querySelectorAll("h1,h2,h3,h4,h5,h6")
-    );
-    setHeadings(headingElements);
-  }, []);
+    const afterElm = document.getElementById('post-body')
+    const headingElements = Array.from(afterElm.querySelectorAll('h1,h2,h3,h4,h5,h6'))
+    setHeadings(headingElements)
+  }, [])
 
   const [activeNode] = useIntersectNode(
-    ["#introduction", ...headings.map((heading) => `#${heading.id}`)],
+    ['#introduction', ...headings.map((heading) => `#${heading.id}`)],
     `0% 0% -55% 0%`,
     1
-  );
+  )
 
   // Function to determine the Heading Level based on `nodeName` (H2, H3, etc)
   const getLevel = (nodeName) => {
-    return Number(nodeName.replace("H", ""));
-  };
+    return Number(nodeName.replace('H', ''))
+  }
 
   return (
     <nav className="toc text-fore-subtle">
@@ -37,17 +35,18 @@ export function TOC() {
             key={heading.id}
             href={`#${heading.id}`}
             className={clsx(
-              "block mt-3 text-sm hover:text-primary-500 focus-visible:outline-primary",
+              'block mt-3 text-sm hover:text-primary-500 focus-visible:outline-primary',
               {
-                "text-primary-500": heading.id === activeNode,
-                "ml-3": getLevel(heading.nodeName) === 3,
+                'text-primary-500': heading.id === activeNode,
+                'ml-2': getLevel(heading.nodeName) === 3,
+                'ml-4': getLevel(heading.nodeName) === 4,
               }
             )}
           >
             {heading.innerText}
           </CustomLink>
-        );
+        )
       })}
     </nav>
-  );
+  )
 }
