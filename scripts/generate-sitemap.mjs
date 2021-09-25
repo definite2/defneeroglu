b4457670e94a7588326df71539a022bf5e1d3b6d
definite2 <edefne878@gmail.com>
2021-09-25 09:23:48 +0300

    update sitemap script
    update sitemap script again
    
    update return condition at sitemap
    
    add ignore patth to sitemap
    
    update generate sitemap, add chnagefrequency
    
    update chnage freq map
    
    update changefreq map again
    
    fix variable name

--- scripts/generate-sitemap.mjs
+++ scripts/generate-sitemap.mjs
@@ -1,37 +1,40 @@
 import { writeFileSync } from 'fs'
 import { globby } from 'globby'
-
 import prettier from 'prettier'
+
 async function generate() {
   const prettierConfig = await prettier.resolveConfig('./.prettierrc.js')
   const pages = await globby([
-    'pages/*.js',
+    'src/pages/**/*.js',
     '_content/blog/**/*.mdx',
     '_content/blog/**/*.md',
     'public/tags/**/*.xml',
-    '!pages/_*.js',
-    '!pages/api',
+    '!src/pages/_*.js',
+    '!src/pages/blog/[...slug].js',
+    '!src/pages/blog/page/[page].js',
+    '!src/pages/404.js',
+    '!src/pages/api',
   ])
-
   const sitemap = `
         <?xml version="1.0" encoding="UTF-8"?>
         <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
             ${pages
               .map((page) => {
                 const path = page
-                  .replace('pages/', '/')
+                  .replace('src/pages/', '/')
                   .replace('_content/blog', '/blog')
                   .replace('public/', '/')
                   .replace('.js', '')
                   .replace('.mdx', '')
                   .replace('/feed.xml', '')
                 const route = path === '/index' ? '' : path
-                if (page === `pages/404.js` || page === `pages/blog/[...slug].js`) {
+                if (page === `src/pages/blog/[...slug].js`) {
                   return
                 }
                 return `
                         <url>
                             <loc>${`https://devmuscle.com${route}`}</loc>
+                            <changefreq>${route === '/blog' ? 'weekly' : 'monthly'}</changefreq>
                         </url>
                     `
               })
