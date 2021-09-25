676bddda5c7460b00020bdb498dfa7028f9207b4
definite2 <edefne878@gmail.com>
2021-09-25 12:48:56 +0300

    use prose

--- src/components/BlogSummaryCard.js
+++ src/components/BlogSummaryCard.js
@@ -1,47 +1,43 @@
-import CustomLink from "./CustomLink";
-import Image from "next/image";
-import Tag from "./Tag";
-import { siteMetadata } from "@/constants/siteMetadata";
+import CustomLink from './CustomLink'
+import Image from 'next/image'
+import Tag from './Tag'
+import { siteMetadata } from '@/constants/siteMetadata'
 const postDateTemplate = { year: 'numeric', month: 'long', day: 'numeric' }
 export default function BlogSummaryCard(props) {
-  const { slug, date, title, summary, tags, image }=props;
+  const { slug, date, title, summary, tags, image } = props
   return (
     <article>
-      <CustomLink
-        href={`/blog/${slug}`}
-        className="text-gray-800 dark:text-gray-100"
-      >
+      <CustomLink href={`/blog/${slug}`} className="text-gray-800 dark:text-gray-100">
         <div className="flex flex-col items-center justify-between h-full overflow-hidden cursor-pointer transition-all duration-200 ease-in-out transform border-2 border-gray-100  dark:border-gray-800 rounded-lg hover:scale-105 hover:shadow-xl">
-          <div className="relative w-full">
-            <header className="relative pb-1/2">
-              <Image
-                layout="fill"
-                className="absolute top-0 left-0 object-cover w-full h-full"
-                src={image}
-                alt={title}
-              />
-            </header>
-          </div>
+          {image && (
+            <div className="relative w-full">
+              <header className="relative pb-1/2">
+                <Image
+                  layout="fill"
+                  className="absolute top-0 left-0 object-cover w-full h-full"
+                  src={image}
+                  alt={title}
+                />
+              </header>
+            </div>
+          )}
           <dl>
             <dt className="sr-only"> Published on</dt>
             <dd className="text-base  leading-6 text-gray-400 dark:text-gray-400">
               <time dateTime={date}>
-                {" "}
-                {new Date(date).toLocaleDateString(
-                  siteMetadata.locale,
-                  postDateTemplate
-                )}{" "}
+                {' '}
+                {new Date(date).toLocaleDateString(siteMetadata.locale, postDateTemplate)}{' '}
               </time>
             </dd>
           </dl>
           <section className="flex flex-col h-full px-5 py-5 sm:px-8 sm:py-10">
-            <h2 className="pb-5 text-xl font-semibold leading-tight sm:text-2xl">
-              {title}
-            </h2>
+            <h2 className="pb-5 text-xl font-semibold leading-tight sm:text-2xl">{title}</h2>
             {/* TODO! fix a descendant of <a> */}
             <div className="flex flex-wrap">
               {tags.map((tag) => (
-                <object key={tag}><Tag key={tag} text={tag} /></object>
+                <object key={tag}>
+                  <Tag key={tag} text={tag} />
+                </object>
               ))}
             </div>
             <div className="prose transition-opacity duration-200 ease-in-out opacity-75 hover:opacity-100 text-gray-500 max-w-none dark:text-gray-400">
@@ -51,6 +47,5 @@ export default function BlogSummaryCard(props) {
         </div>
       </CustomLink>
     </article>
-  );
+  )
 }
-
--- src/pages/index.js
+++ src/pages/index.js
@@ -38,7 +38,7 @@ export default function Home({ posts }) {
 
       <div className="divide-y divide-gray-200 dark:divide-gray-700">
         <div className="pb-10 pt-20 space-y-2 md:space-y-5">
-          <p className="text-lg  leading-7 text-gray-500 dark:text-gray-400">
+          <p className="text-lg leading-7 prose lg:prose-lg dark:prose-dark max-w-none">
             {siteMetadata.description}
           </p>
           <h1 className="text-3xl font-medium leading-9 tracking-tight text-gray-800 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
