83878dd665e058d80183d43cde8f6b868feffd3a
definite2 <edefne878@gmail.com>
2021-09-25 09:23:48 +0300

    update sitemap script

--- scripts/generate-sitemap.mjs
+++ scripts/generate-sitemap.mjs
@@ -5,11 +5,12 @@ import prettier from 'prettier'
 async function generate() {
   const prettierConfig = await prettier.resolveConfig('./.prettierrc.js')
   const pages = await globby([
-    'pages/*.js',
+    'src/pages/**/*.js',
     '_content/blog/**/*.mdx',
     '_content/blog/**/*.md',
     'public/tags/**/*.xml',
-    '!pages/_*.js',
+    '!src/pages/_*.js',
+    '!src/pages/404.js',
     '!pages/api',
   ])
 
@@ -19,14 +20,14 @@ async function generate() {
             ${pages
               .map((page) => {
                 const path = page
-                  .replace('pages/', '/')
+                  .replace('src/pages/', '/')
                   .replace('_content/blog', '/blog')
                   .replace('public/', '/')
                   .replace('.js', '')
                   .replace('.mdx', '')
                   .replace('/feed.xml', '')
                 const route = path === '/index' ? '' : path
-                if (page === `pages/404.js` || page === `pages/blog/[...slug].js`) {
+                if (page === `pages/blog/[...slug].js`) {
                   return
                 }
                 return `
