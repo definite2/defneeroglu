e9137ed5fd16ec7eb37aa81839a9393938de71d7
definite2 <edefne878@gmail.com>
2021-09-26 09:32:04 +0300

    update header style

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,11 +1,11 @@
-import ThemeToggle from "./ThemeToggle";
-import NavLink from "./NavLink";
-import styled, { css } from "styled-components";
-import MobileNavbar from "./MobileNavbar";
-import navLinks from "@/constants/navLinks";
-import React, { useRef } from "react";
-import Logo from "./Logo";
-import useScrolling from "@/hooks/useScroll";
+import ThemeToggle from './ThemeToggle'
+import NavLink from './NavLink'
+import styled, { css } from 'styled-components'
+import MobileNavbar from './MobileNavbar'
+import navLinks from '@/constants/navLinks'
+import React, { useRef } from 'react'
+import Logo from './Logo'
+import useScrolling from '@/hooks/useScroll'
 const Wrapper = styled.nav`
   box-shadow: none;
   transition: box-shadow 250ms ease-in-out;
@@ -15,16 +15,16 @@ const Wrapper = styled.nav`
       box-shadow: rgba(0, 0, 0, 0.15) 0px 1px 4px 0px;
       z-index: 9;
     `};
-`;
+`
 
 const Header = () => {
-  const scrolled = useScrolling(0);
+  const scrolled = useScrolling(0)
   return (
     <Wrapper
       isScrolled={scrolled}
       className="fixed top-0 left-0 w-full flex items-center justify-between border-0 md:grid md:grid-cols-10 gap-1 bg-primary-light dark:bg-gray-900 py-6"
     >
-      <div className="w-full md:col-start-3 md:col-span-6 flex justify-between max-w-3xl px-2 mx-auto sm:px-6 xl:max-w-4xl xl:px-0">
+      <div className="w-full md:col-span-6 md:col-start-3 col-span-10 flex justify-between px-5 ">
         <Logo />
         <div className="flex justify-between items-center text-base leading-5">
           <div className="hidden sm:block">
@@ -39,6 +39,6 @@ const Header = () => {
       </div>
       <MobileNavbar />
     </Wrapper>
-  );
-};
-export default Header;
+  )
+}
+export default Header
