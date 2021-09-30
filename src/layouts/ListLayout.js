b5ff4a2479b7d8c26ce57ee7d164ec40a751e6c6
definite2 <edefne878@gmail.com>
2021-09-30 15:45:46 +0300

    use search svg with object tag

--- src/layouts/ListLayout.js
+++ src/layouts/ListLayout.js
@@ -42,20 +42,7 @@ const ListLayout = ({ posts, title, initialDisplayPosts = [], pagination }) => {
               onChange={(e) => setSearchValue(e.target.value)}
               placeholder="Search articles"
             />
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="w-6 h-6 -ml-8 text-gray-400"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
-              />
-            </svg>
+            <object className="w-6 h-6 -ml-8 text-gray-400" data={'/media/searchIcon.svg'} />
           </div>
         </div>
         <ul className="divide-y divide-gray-200 dark:divide-gray-700">
