f7cff55a567cee94f4683c15846f8bf76501bbbd
definite2 <edefne878@gmail.com>
2021-10-10 15:31:30 +0300

    fix robots text script

--- scripts/generate-robots-txt.js
+++ scripts/generate-robots-txt.js
@@ -1,10 +1,8 @@
-const fs = require('fs');
+const fs = require('fs')
+const crawlableRobotsTxt = `User-agent: *\nAllow: /\n\nSitemap: https://devmuscle.com/sitemap.xml`(
+  function generateRobotsTxt() {
+    // Create a non-crawlable robots.txt in preview links
 
-const crawlableRobotsTxt = `User-agent: *\nAllow: /\n\nSitemap: https://devmuscle.com/sitemap.xml`;
-const uncrawlableRobotsTxt = `User-agent: *\nDisallow: /`;
-(function generateRobotsTxt() {
-  // Create a non-crawlable robots.txt in preview links
-  const robotsTxt =
-    process.env.VERCEL_ENV === 'production' ? crawlableRobotsTxt : uncrawlableRobotsTxt
-  fs.writeFileSync('public/robots.txt', robotsTxt)
-})()
+    fs.writeFileSync('public/robots.txt', crawlableRobotsTxt)
+  }
+)()
