3d820f59f7449e617665d6233ffc1a2c7bb19a55
definite2 <edefne878@gmail.com>
2021-12-18 15:07:54 +0300

    remove scrolled depending padding in main

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -2,20 +2,15 @@ import SectionContainer from '@/components/SectionContainer'
 import Header from '@/components/Header'
 import ScrollToTop from '@/components/ScrollTop'
 import Footer from '@/components/Footer'
-import useScrollingUp from '@/hooks/useScrollingUp'
 
 const Wrapper = ({ children }) => {
-  const scrolled = useScrollingUp()
-
   return (
     <>
       <SectionContainer>
         <Header />
         <main
           role="main"
-          className={`xl:col-span-6 xl:col-start-3 col-span-10 px-5 sm:px-0 my-4 sm:my-12 ${
-            scrolled ? 'pt-20' : '0'
-          }`}
+          className="xl:col-span-6 xl:col-start-3 col-span-10 px-5 sm:px-0 my-4 sm:my-12"
         >
           {children}
         </main>
