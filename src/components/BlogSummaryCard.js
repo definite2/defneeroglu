5dee52480af9fb8bfe97899d2720315e2fd02a92
definite2 <edefne878@gmail.com>
2021-12-26 11:02:05 +0300

    fix accesibility issues

--- src/components/BlogSummaryCard.js
+++ src/components/BlogSummaryCard.js
@@ -2,16 +2,17 @@ import CustomLink from './CustomLink'
 import Image from 'next/image'
 import Tag from './Tag'
 import { siteMetadata } from '@/constants/siteMetadata'
-import { m  } from 'framer-motion'
+import { m } from 'framer-motion'
 const postDateTemplate = { year: 'numeric', month: 'long', day: 'numeric' }
 export default function BlogSummaryCard(props) {
   const { slug, date, title, summary, tags, image } = props
   return (
     <article className="h-full">
       <CustomLink href={`/blog/${slug}`} className="text-gray-800 dark:text-gray-100">
-        <m.div 
-        whileHover={{ scale: 1.05 }}
-        className="flex flex-col items-center justify-between h-full overflow-hidden cursor-pointer border-2 border-gray-150  dark:border-gray-800 rounded-lg">
+        <m.div
+          whileHover={{ scale: 1.05 }}
+          className="flex flex-col items-center justify-between h-full overflow-hidden cursor-pointer border-2 border-gray-150  dark:border-gray-800 rounded-lg"
+        >
           {image && (
             <div className="relative w-full">
               <header className="relative pb-1/2">
@@ -20,29 +21,31 @@ export default function BlogSummaryCard(props) {
                   className="absolute top-0 left-0 object-cover w-full h-full"
                   src={image}
                   alt={title}
+                  priority
                 />
               </header>
             </div>
           )}
           <div className="flex w-full justify-center mt-2">
-          <dl>
-            <dt className="sr-only"> Published on</dt>
-            <dd className="text-base  leading-6 text-gray-400 dark:text-gray-400">
-              <time dateTime={date}>
-                {' '}
-                {new Date(date).toLocaleDateString(siteMetadata.locale, postDateTemplate)}{' '}
-              </time>
-            </dd>
-          </dl>
+            <dl>
+              <dt className="sr-only"> Published on</dt>
+              <dd className="text-base  leading-6 text-gray-400 dark:text-gray-400">
+                <time dateTime={date}>
+                  {' '}
+                  {new Date(date).toLocaleDateString(siteMetadata.locale, postDateTemplate)}{' '}
+                </time>
+              </dd>
+            </dl>
           </div>
           <section className="flex flex-col h-full px-5 py-5 sm:px-8 ">
             <h2 className="pb-5 text-xl font-semibold leading-tight sm:text-2xl">{title}</h2>
             <div className="flex flex-wrap">
-              {tags&&tags.map((tag) => (
-                <object key={tag}>
-                  <Tag key={tag} text={tag} />
-                </object>
-              ))}
+              {tags &&
+                tags.map((tag) => (
+                  <object key={tag} alt={`${tag} tag`}>
+                    <Tag key={tag} text={tag} />
+                  </object>
+                ))}
             </div>
             <div className="prose transition-opacity duration-200 ease-in-out opacity-75 hover:opacity-100 text-gray-500 max-w-none dark:text-gray-400">
               <p>{summary}</p>
