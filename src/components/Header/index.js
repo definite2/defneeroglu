f8002eba6851aff8651c21da4481f210849efb96
Defne Eroglu <defne@appsamurai.com>
2022-04-09 13:05:06 +0300

    fix navbar jump effect

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -9,28 +9,31 @@ import useScrollingUp from '@/hooks/useScrollingUp'
 const Header = () => {
   const scrolled = useScrollingUp()
   return (
-    <nav
-      className={`${
-        scrolled
-          ? 'fixed top-0 left-0 w-full z-10 shadow-nav dark:shadow-md'
-          : 'col-span-12 shadow-none'
-      } transition-shadow duration-300 ease-out flex  items-center justify-between border-0 xl:grid xl:grid-cols-12 gap-1 bg-primary-light dark:bg-gray-900 py-6`}
-    >
-      <div className="w-full xl:col-span-10 xl:col-start-2 flex justify-between px-5 sm:px-0 ">
-        <Logo />
-        <div className="flex justify-between items-center text-base leading-5">
-          <div className="hidden sm:block">
-            {navLinks.map((route, i) => (
-              <NavLink key={i} href={route.href}>
-                {route.title}
-              </NavLink>
-            ))}
+    <>
+      <nav
+        className={`${
+          scrolled
+            ? 'fixed top-0 left-0 w-full z-10 shadow-nav dark:shadow-md'
+            : 'col-span-12 shadow-none'
+        } max-h-24 transition-shadow duration-300 ease-out flex  items-center justify-between border-0 xl:grid xl:grid-cols-12 gap-1 bg-primary-light dark:bg-gray-900 py-6`}
+      >
+        <div className="w-full xl:col-span-10 xl:col-start-2 flex justify-between px-5 sm:px-0 ">
+          <Logo />
+          <div className="flex justify-between items-center text-base leading-5">
+            <div className="hidden sm:block">
+              {navLinks.map((route, i) => (
+                <NavLink key={i} href={route.href}>
+                  {route.title}
+                </NavLink>
+              ))}
+            </div>
+            <ThemeToggle />
           </div>
-          <ThemeToggle />
         </div>
-      </div>
-      <MobileNavbar />
-    </nav>
+        <MobileNavbar />
+      </nav>
+      <div style={{ height: 100 }} className={`${scrolled ? 'block' : 'hidden'} col-span-12`}></div>
+    </>
   )
 }
 export default Header
